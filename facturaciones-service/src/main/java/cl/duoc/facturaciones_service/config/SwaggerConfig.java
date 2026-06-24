684e95a575a5bf1dfb8dfb81add907dc0eb85dce
TitoChemen <ke.sosa@duocuc.cl>
2026-06-24 02:28:30 -0400

    faltaban 2 lineas de codigo y funcionaba el swagger xd

--- facturaciones-service/src/main/java/cl/duoc/facturaciones_service/config/SwaggerConfig.java
+++ facturaciones-service/src/main/java/cl/duoc/facturaciones_service/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package cl.duoc.facturaciones_service.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +11,10 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .info(new Info()
                         .title("Facturaciones")
                         .version("1.0.0")
                         .description("Documentación de los endpoints del sistema de Facturaciones."));
     }
 }
-
